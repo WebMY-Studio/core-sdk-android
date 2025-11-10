ec3dc7dc4a1b15ae1f2d083064a4a1705650f342
Mikhail Yarashevich <bigbottleapps@gmail.com>
2025-11-10 23:07:16 +0100

    Release v0.1.7

--- core-sdk/src/main/java/com/webmy/core_sdk/tools/ads/AdsPremiumManager.kt
+++ core-sdk/src/main/java/com/webmy/core_sdk/tools/ads/AdsPremiumManager.kt
@@ -4,56 +4,73 @@ import android.app.Activity
 import android.widget.FrameLayout
 import com.webmy.core_sdk.tools.billing.BillingManager
 import com.webmy.core_sdk.tools.billing.containsPurchased
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 interface AdsPremiumManager {
     val isPremiumFlow: Flow<Boolean>
 
-    suspend fun requestBanner(
+    fun requestBanner(
         activity: Activity,
         container: FrameLayout,
     )
 
-    suspend fun requestReward(
+    fun requestReward(
         activity: Activity,
         placement: String? = null,
         rewardCallback: (Boolean) -> Unit,
     )
 
-    suspend fun requestInterstitial(activity: Activity)
+    fun requestInterstitial(activity: Activity)
 }
 
 class RealAdsPremiumManager(
     premiumProductId: String,
     billingManager: BillingManager,
     private val adsManager: AdsManager,
-) : AdsPremiumManager {
+) : AdsPremiumManager, CoroutineScope {
+
+    override val coroutineContext: CoroutineContext = Dispatchers.IO
 
     override val isPremiumFlow = billingManager.productsFlow
         .map { it.containsPurchased(premiumProductId) }
 
-
-    override suspend fun requestBanner(activity: Activity, container: FrameLayout) {
-        if (isPremiumFlow.first()) {
-            adsManager.hideBanner(activity, container)
-        } else {
-            adsManager.showBanner(activity, container)
+    override fun requestBanner(activity: Activity, container: FrameLayout) {
+        launch {
+            val isPremium = isPremiumFlow.first()
+            withContext(Dispatchers.Main) {
+                if (isPremium) {
+                    adsManager.hideBanner(activity, container)
+                } else {
+                    adsManager.showBanner(activity, container)
+                }
+            }
         }
     }
 
-    override suspend fun requestInterstitial(activity: Activity) {
-        if (!isPremiumFlow.first()) {
-            adsManager.showInter(activity)
+    override fun requestInterstitial(activity: Activity) {
+        launch {
+            val isPremium = isPremiumFlow.first()
+            withContext(Dispatchers.Main) {
+                if (!isPremium) {
+                    adsManager.showInter(activity)
+                }
+            }
         }
     }
 
-    override suspend fun requestReward(
+    override fun requestReward(
         activity: Activity,
         placement: String?,
         rewardCallback: (Boolean) -> Unit,
     ) {
-        return adsManager.showReward(activity, placement, rewardCallback)
+        adsManager.showReward(activity, placement, rewardCallback)
     }
+
 }
