5c134793458003d005bec4267168de521a539257
Mike Y <bigbottleapps@gmail.com>
2026-06-02 14:26:41 +0200

    Release v0.5.12

--- core-monetization/src/main/java/us/webmy/core/monetization/billing/domain/interactor/PremiumUseCase.kt
+++ core-monetization/src/main/java/us/webmy/core/monetization/billing/domain/interactor/PremiumUseCase.kt
@@ -4,11 +4,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import us.webmy.core.monetization.billing.tools.billing.BillingManager
-import us.webmy.core.monetization.billing.tools.billing.Product
 
 interface PremiumUseCase {
-
-    val subscriptionsFlow: Flow<List<Product.Subscription>>
 
     val isPremiumFlow: Flow<Boolean>
 }
@@ -20,15 +17,10 @@ internal class RealPremiumUseCase(
     private val premiumProductIds: Set<String>,
 ) : PremiumUseCase {
 
-    override val subscriptionsFlow = billingManager.subscribeProducts()
-        .map { it.filterIsInstance<Product.Subscription>() }
-
     override val isPremiumFlow = billingManager.subscribeProducts()
-        .map { products ->
-            if (premiumProductIds.isEmpty()) {
-                products.any { it.isPurchased }
-            } else {
-                products.any { p -> p.isPurchased && p.id in premiumProductIds }
+        .map {
+            it.any { product ->
+                product.isPurchased && product.id in premiumProductIds
             }
         }
 }
