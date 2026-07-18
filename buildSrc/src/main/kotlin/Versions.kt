6e838b4c8c179f1c21a54033b7d51f1eda2e399c
Mike Y <bigbottleapps@gmail.com>
2026-07-19 01:16:51 +0200

    Release v0.5.16

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ import org.gradle.api.Project
 const val CompileSdkVersion = 36
 const val MinSdkVersion = 28
 const val TargetSdkVersion = 36
-private const val DefaultVersionName = "0.5.15"
+private const val DefaultVersionName = "0.5.16"
 private val DefaultVersionCode = (System.currentTimeMillis() / 1000).toInt()
 
 fun Project.computeVersionName(): String = DefaultVersionName
