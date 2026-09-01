3c4d1e8bf514ea480a451b44ca1bc73015dcce6a
Mike Y <bigbottleapps@gmail.com>
2026-09-01 17:21:48 +0200

    Release 1.0.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 import org.gradle.api.Project
 
-const val CompileSdkVersion = 36
+const val CompileSdkVersion = 37
 const val MinSdkVersion = 28
-const val TargetSdkVersion = 36
-private const val DefaultVersionName = "1.0.2"
+const val TargetSdkVersion = 37
+private const val DefaultVersionName = "1.0.3"
 private val DefaultVersionCode = (System.currentTimeMillis() / 1000).toInt()
 
 fun Project.computeVersionName(): String = DefaultVersionName
